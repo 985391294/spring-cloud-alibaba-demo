a50431a1476e53a712f3e3f20203f8964f825467
985391294@qq.com <985391294@qq.com>
2021-03-01 18:08:18 +0800

    提交全局swagger文档

--- cloud-gateway/src/main/java/com/tqz/gateway/GatewayApplication.java
+++ cloud-gateway/src/main/java/com/tqz/gateway/GatewayApplication.java
@@ -18,30 +18,40 @@ import org.springframework.cloud.gateway.handler.predicate.*;
  * 一共有以下12大工厂 <a herf:"https://cloud.spring.io/spring-cloud-static/spring-cloud-gateway/2.1.0.RELEASE/single/spring-cloud-gateway.html#gateway-request-predicates-factories"></a>
  * <ul>
  *     <li>AfterRoutePredicateFactory 当请求时的时间After配置的时间时，才转发该请求。
- *     示例：- After=2018-08-16T11:34:42.917822900+08:00[Asia/Shanghai]</li>
+ *     示例：- After=2018-08-16T11:34:42.917822900+08:00[Asia/Shanghai]
+ *     </li>
  *     <li>BeforeRoutePredicateFactory 当请求时的时间Before配置的时间时，才转发该请求。
- *     示例：- Before=2018-08-16T11:34:42.917822900+08:00[Asia/Shanghai]</li>
+ *     示例：- Before=2018-08-16T11:34:42.917822900+08:00[Asia/Shanghai]
+ *     </li>
  *     <li>BetweenRoutePredicateFactory 当请求时的时间Between配置的时间段时，才转发该请求。
  *     示例：- Between=2018-08-16T11:34:42.917822900+08:00[Asia/Shanghai], 2028-08-16T11:34:42.917822900+08:00[Asia/Shanghai]</li>
- *     <li>CloudFoundryRouteServiceRoutePredicateFactory 暂未官方文档说明</li>
+ *     <li>CloudFoundryRouteServiceRoutePredicateFactory 暂未官方文档说明
+ *     </li>
  *     <li>CookieRoutePredicateFactory 当请求时携带的Cookie名称及值与配置的名称及值相符时，才转发该请求。
  *     示例：- Cookie=chocolate, ch.p （当且仅当请求带有名为chocolate，并且值符合正则表达式 ch.p 的Cookie时，才转发该请求）</li>
  *     <li>HeaderRoutePredicateFactory 当请求时携带的Header名称及值与配置的名称及值相符时，才转发该请求。
  *     示例：- Header=X-Request-Id, \d+   （当且仅当请求带有名为X-Request-Id，并且值符合正则表达式 \d+ 的Header时，才转发该请求）
  *     </li>
  *     <li>HostRoutePredicateFactory 当请求时名为Host的Header的值与配置的值相符时，才转发该请求。
- *     示例：- Host=**.somehost.org,**.anotherhost.org </li>
+ *     示例：- Host=**.somehost.org,**.anotherhost.org
+ *     </li>
  *     <li>MethodRoutePredicateFactory 当请求时所使用的HTTP方法与配置的请求方法相符时，才转发该请求。
- *     示例：- Method=GET</li>
+ *     示例：- Method=GET
+ *     </li>
  *     <li>PathRoutePredicateFactory 当请求时所访问的路径与配置的路径相匹配时，才转发该请求。
- *     示例：- Path=/foo/{segment},/example/list,/bar/** </li>
- *     <li>ReadBodyPredicateFactory 暂未官方文档说明 </li>
+ *     示例：- Path=/foo/{segment},/example/list,/bar/**
+ *     </li>
+ *     <li>ReadBodyPredicateFactory 暂未官方文档说明
+ *     </li>
  *     <li>QueryRoutePredicateFactory 当请求时所带有的参数名称与配置的参数名称相符时，才转发该请求。
- *     示例：- Query=baz  （当且仅当请求带有名为baz的参数，才转发该请求）</li>
+ *     示例：- Query=baz  （当且仅当请求带有名为baz的参数，才转发该请求）
+ *     </li>
  *     <li>RemoteAddrRoutePredicateFactory 当请求时的IP地址与配置的IP地址相符时，才转发该请求。
- *     示例：- RemoteAddr=192.168.1.1/24 </li>
+ *     示例：- RemoteAddr=192.168.1.1/24
+ *     </li>
  *     <li>WeightRoutePredicateFactory 路由组和权重，才转发该请求。
- *     示例：- Weight=weightGroup,3 </li>
+ *     示例：- Weight=weightGroup,3
+ *     </li>
  * </ul>
  * </p>
  *
